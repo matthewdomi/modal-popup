fa4710a23bf206a9b3700ac5d752f668ff842757
Matthew Domi <70774927+matthewdomi@users.noreply.github.com>
2023-01-09 21:46:11 +0000

    nice

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import Modal from '../components/Modal';
 export default function ModalCreen() {
   const [showModal, setShowModal] = useState();
   return (
-    <div>
+    <div className="flex justify-center items-center ">
       <h2>Modal</h2>
       <button className="primary-button" onClick={() => setShowModal(true)}>
         Mod
